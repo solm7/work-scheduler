85abd323af74153c67027003f77c96d0fcc0ac5a
solm7 <105757874+solm7@users.noreply.github.com>
2022-11-10 16:20:58 -0500

    Added Time Block function

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,29 @@ const saveButton = $(".saveBtn");
 
 currentDay.textContent(moment().format("ddd MMM Do YYYY"));
 
+// time block function for moments in time
+ timeBlock() = () => {
+    const hour = moment().hours();
+
+    $(".time-block").each(function() {
+        const currentHour = parseInt($(this).attr("id"));
+
+        if (currentHour === hour) {
+            $(this).addClass("present");
+        }
+        else if (currentHour < hour) {
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (currentHour > hour) {
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+};
+  // var currentHour = parseInt($("#currentDay").format("h"));
+        // var currentHour = parseInt(currentDateTime.format("h"));
+
 // save event in local storage
 saveBtn.on("click", function () {
   var timeOfDay = $(this).siblings(".hour").text();
