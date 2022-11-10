f659a56376357e9e1d14284d7e2c9b2442a36734
solm7 <105757874+solm7@users.noreply.github.com>
2022-11-10 16:09:47 -0500

    Local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,13 @@
+let currentDay = document.querySelector("#currentDay");
+
 const saveButton = $(".saveBtn");
 
-$("#currentDay").text(moment().format("ddd MMM Do YYYY"));
+currentDay.textContent(moment().format("ddd MMM Do YYYY"));
+
+// save event in local storage
+saveBtn.on("click", function () {
+  var timeOfDay = $(this).siblings(".hour").text();
+  var activity = $(this).siblings(".activity").val();
+
+  localStorage.setItem(timeOfDay, activity);
+});
